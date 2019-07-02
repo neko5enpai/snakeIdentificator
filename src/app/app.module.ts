b07dc249576e31b9b3458fd4d18e1ef922d509e0
Neko5enpai <vmonod7@gmail.com>
2019-07-02 15:42:34 +0200

    Ajout des components de la navigation inférieure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { NavComponent } from './nav/nav.component';
+import { ObservationsComponent } from './observations/observations.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { CommunauteComponent } from './communaute/communaute.component';
+import { ParametresComponent } from './parametres/parametres.component';
+import { BottomnavComponent } from './bottomnav/bottomnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    NavComponent,
+    ObservationsComponent,
+    DocumentationComponent,
+    CommunauteComponent,
+    ParametresComponent,
+    BottomnavComponent
   ],
   imports: [
     BrowserModule,
